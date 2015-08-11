6f92ed2645e54321b81f1d2cdc3e10144b727f09
Christophe Willemsen <willemsen.christophe@gmail.com>
2015-08-11 03:12:19 +0200

    initial commit

---
+++ src/app.php
@@ -0,0 +1,35 @@
+<?php
+
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\RoutingServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+use Neoxygen\NeoClient\ClientBuilder;
+
+$app = new Application();
+$app->register(new RoutingServiceProvider());
+$app->register(new ValidatorServiceProvider());
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new TwigServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+$app['twig'] = $app->extend('twig', function ($twig, $app) {
+    // add custom globals, filters, tags, ...
+
+    $twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) use ($app) {
+        return $app['request_stack']->getMasterRequest()->getBasepath().'/'.ltrim($asset, '/');
+    }));
+
+    return $twig;
+});
+$app['neo'] = function(){
+  $client = ClientBuilder::create()
+    ->addConnection('default', 'http', 'localhost', 7474, true, 'neo4j', 'veryCoolMax')
+    ->setAutoFormatResponse(true)
+    ->build();
+
+    return $client;
+};
+
+return $app;
---
+++ src/controllers.php
@@ -0,0 +1,91 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+//Request::setTrustedProxies(array('127.0.0.1'));
+
+$app->get('/', function() use($app) {
+    return $app['twig']->render('index.html.twig');
+})
+    ->bind('welcome');
+
+$app->get('/thankyou', function() use ($app) {
+    return $app['twig']->render('success.html.twig');
+});
+
+$app->post('/register', function (Request $request) use ($app) {
+    $email = $request->request->get('user_email');
+    $loc = $request->getPreferredLanguage();
+    $locs = $request->getLanguages();
+    $locales = [];
+    foreach ($locs as $k => $l) {
+        $locales[] = ['code' => $l, 'priority' => $k];
+    }
+    $ip = $request->getClientIp();
+
+
+    $query = 'MERGE (registration:Registration {time:{time}})
+    MERGE (user:User {email:{email}})
+    SET user.ip = {ip}
+    MERGE (registration)-[:FROM_USER]->(user)
+    WITH user
+    UNWIND {languages} as l
+    MERGE (lg:Language {code: l.code})
+    MERGE (user)-[:BROWSER_LANGUAGE {priority: l.priority}]->(lg)
+    WITH user
+    MERGE (pl:Language {code: {pl}})
+    MERGE (user)-[:PREFERED_LANGUAGE]->(pl)';
+    $p = [
+        'time' => (time() * 1000),
+        'email' => trim((string) strtolower($email)),
+        'ip' => $ip,
+        'languages' => $locales,
+        'pl' => $loc,
+    ];
+
+    try {
+        $neo = $app['neo'];
+        $neo->sendCypherQuery($query, $p);
+    } catch (\Neoxygen\NeoClient\Exception\Neo4jException $e) {
+        // ...
+    }
+
+    try {
+        $msg = Swift_Message::newInstance();
+        $msg->setSubject('New registration');
+        $msg->setFrom(array('announce@octifyapp.com' => 'octify announce'));
+        $msg->setTo(array('willemsen.christophe@gmail.com' => 'Christophe Willemsen'));
+        $msg->setBody('New registration for Octify with email ' . $email);
+        $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+          ->setUsername('octifylabz@gmail.com')
+          ->setPassword('error!2101CWX');
+        $mailer = Swift_Mailer::newInstance($transport);
+        $mailer->send($msg);
+    } catch (Swift_TransportException $e) {
+        // ...
+    }
+
+    return $app->redirect('/');
+})
+->bind('register')
+;
+
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
+
+    // 404.html, or 40x.html, or 4xx.html, or error.html
+    $templates = array(
+        'errors/'.$code.'.html.twig',
+        'errors/'.substr($code, 0, 2).'x.html.twig',
+        'errors/'.substr($code, 0, 1).'xx.html.twig',
+        'errors/default.html.twig',
+    );
+
+    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+});
